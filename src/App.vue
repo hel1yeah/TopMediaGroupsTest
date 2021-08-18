67b7e9df8e9f7e11390270028c1646ff03363ffe
hel1yeah <hel1_yeah@ukr.net>
2021-08-18 21:15:33 +0300

    feat: added bg page, blur and dark

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,42 @@
 <template>
-  <p>
-    Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные
-    тексты. Обеспечивает, они! Раз жизни своего рукопись ipsum там. Большой
-    взобравшись журчит всеми подзаголовок пор заглавных, парадигматическая там
-    дорогу дороге грустный.
-  </p>
+  <section class="form">
+    <div class="form-bg"></div>
+    <div class="container"></div>
+  </section>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.form {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  .form-bg {
+    position: absolute;
+    filter: blur(15px);
+    top: -5%;
+    left: -5%;
+    width: 110%;
+    height: 110%;
+    background: {
+      image: url("./assets/images/bg.jpg");
+      position: top top;
+      size: cover;
+      repeat: no-repeat;
+    }
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: #000;
+    opacity: 0.6;
+  }
+}
+</style>
