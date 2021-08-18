1440bd9ce67e2480a72125c6380069a5876690e5
hel1yeah <hel1_yeah@ukr.net>
2021-08-18 20:40:04 +0300

    feat: added fonts, setting global styles

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
 
+import "normalize.css/normalize.css";
+import "./assets/scss/main.scss";
+
 createApp(App).use(store).mount("#app");
