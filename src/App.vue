069f29ac2e7a9fc55de733b1efc632d47a83ab29
hel1yeah <hel1_yeah@ukr.net>
2021-08-18 23:16:23 +0300

    fix: create use plaseholder

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,10 @@
         rest while traveling
       </h1>
       <form class="form">
-        <input type="text" class="form-input" />
+        <label class="form-lable">
+          <input type="text" class="form-input" />
+          <span class="form-input-placeholder">First Name</span>
+        </label>
       </form>
     </div>
   </section>
@@ -65,13 +68,48 @@ export default {};
   color: var(--color-secondary);
 }
 .form {
+  font-family: "Rubik-Light", sans-serif;
+  font-size: 19px;
 }
-
-.form-input {
+.form-lable {
+  position: relative;
   width: 300px;
   height: 56px;
+  display: block;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 15px;
+    left: 16px;
+    width: 24px;
+    height: 24px;
+    background: url("./assets/images/user-name.svg");
+  }
+}
+.form-input-placeholder {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 50px;
+  transition: 0.3s;
+}
+.form-input {
+  width: 100%;
+  height: 100%;
+  padding: 0 5px 0 50px;
   background: transparent;
+  position: relative;
+  color: var(--color-quinary);
+  border-radius: 5px;
   border: 1px solid var(--color-primary);
   box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.3);
+  outline: transparent;
+
+  &:focus + .form-input-placeholder {
+    font-size: 11px;
+    color: var(--color-secondary);
+    top: 8px;
+    transform: translateY(0px);
+  }
 }
 </style>
